cc8a27f282212ac88e75640343e525eba1dc5e1e
Tanya <tanyasinha828@gmail.com>
2023-07-07 20:52:04 +0530

    Remove font

--- marksheet-analysis-system/src/app/layout.js
+++ marksheet-analysis-system/src/app/layout.js
@@ -1,8 +1,5 @@
 import React from "react";
 import "./globals.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Marksheet Analysis System",
@@ -12,7 +9,7 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 };
