19efac0886b8774c2fd9355d30abb011a1c52422
Tanya <tanyasinha828@gmail.com>
2023-07-11 12:05:01 +0530

    Revret

--- marksheet-analysis-system/src/app/search/page.js
+++ marksheet-analysis-system/src/app/search/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import React, { useState } from "react";
 import supabase from "../supabase";
 import ScoreTable from "../ScoreTable";
 import Navbar from "../Navbar";
